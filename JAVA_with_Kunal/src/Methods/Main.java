8493d4506cb7c4a8ee3878510175d6753a2da087
Khushi Gangwar <khushigangwar344@gmail.com>
2024-09-08 17:25:06 +0530

    initial commit

---
+++ JAVA_with_Kunal/src/Methods/Main.java
@@ -0,0 +1,19 @@
+package Methods;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter first num:");
+		int a = sc.nextInt();
+		System.out.println("Enter second num:");
+		int b= sc.nextInt();
+		int sum= a+b;
+		System.out.println(sum);
+			
+	
+	
+	
+	}
+}	
---
+++ JAVA_with_Kunal/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JAVA_with_Kunal {
+}
